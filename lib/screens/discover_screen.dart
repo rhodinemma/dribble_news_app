37ff5f20dccbfd4df1f2c81d70716bf58bde2132
rhodinemma <rhodinemma10@gmail.com>
2022-10-31 15:48:01 +0300

    add resource files

---
+++ lib/screens/discover_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DiscoverScreen extends StatelessWidget{
+  const DiscoverScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return const Scaffold();
+  }
+}
