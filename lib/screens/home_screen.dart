337b132fcc70e33ee8d34c7beac9f4d060286d3e
rhodinemma <rhodinemma10@gmail.com>
2022-10-31 18:12:51 +0300

    build breakingNews section

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,7 +30,79 @@ class HomeScreen extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.zero,
         children: [
-          _NewsOfTheDay(article: article)
+          _NewsOfTheDay(article: article),
+          const SizedBox(height: 20),
+          _BreakingNews(articles: Article.articles)
+        ],
+      ),
+    );
+  }
+}
+
+class _BreakingNews extends StatelessWidget {
+  const _BreakingNews({
+    Key? key,
+    required this.articles,
+  }) : super(key: key);
+
+  final List<Article> articles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Breaking News',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'More',
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          SizedBox(
+            height: 250,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: articles.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ImageContainer(
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            imageUrl: articles[index].imageUrl),
+                        const SizedBox(height: 10),
+                        Text(
+                          articles[index].title,
+                          maxLines: 2,
+                          style: Theme.of(context).textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.bold, height: 1.5),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                            '${DateTime.now().difference(articles[index].createdAt).inHours} hours ago',
+                            style: Theme.of(context).textTheme.bodySmall),
+                        const SizedBox(height: 5),
+                        Text('by ${articles[index].author}',
+                            style: Theme.of(context).textTheme.bodySmall),
+                      ],
+                    ),
+                  );
+                }),
+          )
         ],
       ),
     );
@@ -56,24 +128,20 @@ class _NewsOfTheDay extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CustomTag(
-              backgroundColor: Colors.grey.withAlpha(150),
-              children: [
-                Text(
-                  'News of the day',
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyMedium!
-                      .copyWith(color: Colors.white),
-                )
-              ]),
+          CustomTag(backgroundColor: Colors.grey.withAlpha(150), children: [
+            Text(
+              'News of the day',
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyMedium!
+                  .copyWith(color: Colors.white),
+            )
+          ]),
           const SizedBox(height: 10),
           Text(
             article.title,
             style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                fontWeight: FontWeight.bold,
-                height: 1.25,
-                color: Colors.white),
+                fontWeight: FontWeight.bold, height: 1.25, color: Colors.white),
           ),
           TextButton(
               onPressed: () {},
@@ -87,8 +155,13 @@ class _NewsOfTheDay extends StatelessWidget {
                         .bodyLarge!
                         .copyWith(color: Colors.white),
                   ),
-                  const SizedBox(width: 10,),
-                  const Icon(Icons.arrow_right_alt, color: Colors.white,)
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  const Icon(
+                    Icons.arrow_right_alt,
+                    color: Colors.white,
+                  )
                 ],
               ))
         ],
